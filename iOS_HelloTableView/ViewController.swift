83a316b1473fd4a0f1895c24f7413afc148857ad
Richy <richy@webcomm.com.tw>
2023-07-27 13:48:33 +0800

    協定實作 numberOfSections, numberOfRowsInSection, cellForRowAt

--- iOS_HelloTableView/ViewController.swift
+++ iOS_HelloTableView/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 // UITableViewDataSource, UITableViewDelegate 協定
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // 實作section (區塊)
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Row number
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // Row cell 每一列的內容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "iPhone"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
