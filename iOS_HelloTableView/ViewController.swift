410da2612f941d95593682dc14651c606544e883
Richy <richy@webcomm.com.tw>
2023-07-27 18:55:13 +0800

    UITableView.dequeueReusableCell(), indexPath.section, indexPath.row

--- iOS_HelloTableView/ViewController.swift
+++ iOS_HelloTableView/ViewController.swift
@@ -17,15 +17,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // Row number
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        // return 3
+        return fruitArray.count
     }
     
     // Row cell 每一列的內容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "iPhone"
+        // let cell = UITableViewCell()
+        // 滑動到銀幕之外的cell，就不需要在佔用一個cell，由下方其他的cell遞補。可能是回收的或是新的cell，避免資料太多時佔用太多記憶體。
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // indexPath.section // 哪個區塊
+        
+        cell.textLabel?.text = fruitArray[indexPath.row] // "iPhone"
+        
         return cell
     }
+    
+    // data Array
+    var fruitArray = ["apple", "banana", "mango", "watermelon"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
