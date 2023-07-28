bbe0183cdc3129680f9143f2097ee17863f3f9b8
Richy <richy@webcomm.com.tw>
2023-07-28 11:03:40 +0800

    set TableView data source

--- iOS_HelloTableView/AnimalTableViewController.swift
+++ iOS_HelloTableView/AnimalTableViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class AnimalTableViewController: UITableViewController {
+    
+    let animalArray = ["cat", "chicken", "dog", "elephant", "fox", "goat", "kangaroo", "monkey", "mouse", "penguin", "rabbit", "snail"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,23 +26,23 @@ class AnimalTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return animalArray.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
         // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) // reuseIdentifier
+        cell.textLabel?.text = animalArray[indexPath.row]
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
