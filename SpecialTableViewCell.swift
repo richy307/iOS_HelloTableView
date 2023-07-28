5b82df7367c9c42b54815c718013c3b8376cc645
Richy <richy@webcomm.com.tw>
2023-07-28 14:56:33 +0800

    create SpecialTableViewCell.swift

---
+++ SpecialTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SpecialTableViewCell.swift
+//  iOS_HelloTableView
+//
+//  Created by 王麒翔 on 2023/7/28.
+//
+
+import UIKit
+
+class SpecialTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
